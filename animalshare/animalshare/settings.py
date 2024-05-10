3e2e05857db5363208c149485237796b7cd9fd56
ParanoyaFobios <237629134pah@gmail.com>
2024-05-10 10:29:54 +0300

    change to waitress

--- animalshare/animalshare/settings.py
+++ animalshare/animalshare/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = False
 
-ALLOWED_HOSTS = ['*'] #поменять перед деплоем
+ALLOWED_HOSTS = ['*', 'animalshare.herokuapp.com', 'localhost'] #поменять перед деплоем
 
 
 # Application definition
