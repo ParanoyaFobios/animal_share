6a537ea67c13e9a5c4396767afb0cd039b268a81
ParanoyaFobios <237629134pah@gmail.com>
2024-05-10 10:42:39 +0300

    random comm

--- animalshare/animalshare/settings.py
+++ animalshare/animalshare/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', 'animalshare.herokuapp.com', 'localhost'] #поменять перед деплоем
+ALLOWED_HOSTS = ['*.herokuapp.com', 'localhost'] #поменять перед деплоем
 
 
 # Application definition
