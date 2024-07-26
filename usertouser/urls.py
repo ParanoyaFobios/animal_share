e3b5234d184a70c8614daad30f0952e21d6f583b
ParanoyaFobios <237629134pah@gmail.com>
2024-07-26 15:09:02 +0300

    sms fixed 28

--- usertouser/urls.py
+++ usertouser/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import usertouserCreateView, usertouserListView, usertouserDetailListView
+from .views import usertouserCreateView, usertouserListView, usertouserDetailListView, usertouserOutboxListView
 
 
 
 urlpatterns = [
 path('sms/inbox/', usertouserListView.as_view(template_name='messages/inbox.html'), name='inbox'),
 path('sms/create/<str:recipient>/', usertouserCreateView.as_view(), name='create_message'),
-path('sms/outbox/', usertouserListView.as_view(template_name='messages/outbox.html'), name='outbox'),
+path('sms/outbox/', usertouserOutboxListView.as_view(template_name='messages/outbox.html'), name='outbox'),
 path('sms/<int:pk>/', usertouserDetailListView.as_view(), name='detail'),
 ]
