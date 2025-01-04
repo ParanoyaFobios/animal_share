0d1e6b89de972f523955f4e7dd07d9a3b47efd66
ParanoyaFobios <237629134pah@gmail.com>
2025-01-04 16:57:21 +0200

    product models adedd

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Categories, Products
 
+admin.site.register(Categories)
+admin.site.register(Products)
 # Register your models here.
--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=150, unique=True, blank=True, null=True, verbose_name = 'URL')
+
+    class Meta:
+        db_table = 'category' #переименование таблицы в БД из Categories в category
+        verbose_name = 'Категорию' #альтернативное имя для отображения в админке
+        verbose_name_plural = 'Категории' #тоже самое, но с указанием множественного числа
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=150, unique=True, blank=True, null=True, verbose_name = 'URL')
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(default='default.jpg', upload_to='catalog_pics')
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2)
+    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(to=Categories, on_delete = models.SET_DEFAULT, default='Other')#при удалении категории, все товары, которые остались ней, будут перемещены в категорию other, а не удалены каскадно, вслед за категорией
+
+
+    class Meta:
+        db_table = 'product'
