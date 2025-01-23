a5bb0271c4273657ddc05fa3dc02dd576a5a418c
ParanoyaFobios <237629134pah@gmail.com>
2025-01-23 14:43:02 +0200

    Delete cart option added

--- carts/urls.py
+++ carts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('users-cart/', views.users_cart, name='users-cart'),
     path('cart_add/<slug:product_slug>/', views.cart_add, name='cart-add'),
     path('cart_change/<slug:product_slug>/', views.cart_change, name='cart-change'),
-    path('cart_remove/<slug:product_slug>', views.cart_remove, name='cart-remove'), #задействуем URL dispatcher см документацию джанго
+    path('cart_remove/<int:cart_id>', views.cart_remove, name='cart-remove'), #задействуем URL dispatcher см документацию джанго
     ]
