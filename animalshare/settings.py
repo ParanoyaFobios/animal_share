f8451c3b95054b52e6067d1e472411bf5d12268e
ParanoyaFobios <237629134pah@gmail.com>
2026-04-13 22:38:56 +0300

    moove to UV

--- animalshare/settings.py
+++ animalshare/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['animalshare-34ce92dca6ea.herokuapp.com', '127.0.0.1'] #поменять перед деплоем
-CSRF_TRUSTED_ORIGINS = ['https://animalshare-34ce92dca6ea.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'animalshare-34ce92dca6ea.herokuapp.com'] #поменять перед деплоем
+CSRF_TRUSTED_ORIGINS = ['https://animalshare-34ce92dca6ea.herokuapp.com', 'http://127.0.0.1:8000', 'http://localhost:8000']
 
 # Application definition
 
